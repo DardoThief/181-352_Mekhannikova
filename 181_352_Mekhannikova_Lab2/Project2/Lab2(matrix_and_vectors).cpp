8b13278739e3cd9eedcb965c6c059dc6a46df67e
temp <temp@STUD.UNV.SU>
2018-10-11 12:19:11 +0300

    11.10.2018 12:19:10,95

--- 181_352_Mekhannikova_Lab2/Project2/Lab2(matrix_and_vectors).cpp
+++ 181_352_Mekhannikova_Lab2/Project2/Lab2(matrix_and_vectors).cpp
@@ -1,4 +1,4 @@
-//произведение матрицы на матрицу
+
 #include "stdafx.h"
 #include <iostream>
 #include <string>
@@ -14,6 +14,8 @@ int main()
 						{26, 27, 28} };
 
 	int cResult[3][3] = {0};
+
+	//произведение матрицы на матрицу
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -24,57 +26,53 @@ int main()
 			}
 			std::cout << cResult[i][j] << "\t";
 		}
-		std::cout << "\n";
+		std::cout << "\n"; 
 	}
-	getchar();
-	return 0;
+
 
 //произведение матрицы на вектор
-
-
-	int Vector[3], sum, MatrixC[3][3], a;
-	for (int i = 0; i < 3; i++) 
-	{
-		std::cout << Vector[3] << std::endl;
-	}
-
+	int Vector[3] = {3, 2, 3};
+	int sum = 0;
+	int MatrixC[3][3] = { { 3, 3, 3 },
+						{ 2, 2, 2 },
+						{ 1, 1, 1 } };
+	int a = 0;
 	for (int i = 0; i < 3; i++) 
 	{
 		for (int k = 0; k < 3; k++) 
 		{
-			a = Vector[3] * MatrixC[3][3];
+			a = Vector[i] * MatrixC[i][k];
 			sum = sum + a;
 		}
-		Vector[3] = sum;
+		Vector[i] = sum;
 		sum = 0;
 		std::cout << Vector[3] << std::endl;
 	}
-	getchar();
-	return 0;
 
 
 //поэлементное произведение матриц
-	int arr1[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int arr2[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	int arr3[10] = {0};
+	int cMatrix[3][2] = { { 1, 4 },{ 2, 5 },{ 3, 6 } };
+	int dMatrix[2][3] = { { 7, 8, 9 },{ 10, 11, 12 } };
+	int product[3][3] = { { 0, 0, 0 },{ 0, 0, 0 },{ 0, 0, 0 } };
 
-	for (int i = 0; i < 10; i++)
+	for (int row = 0; row < 3; row++) 
 	{
-		std::cout << arr1[10] << " * ";
-
-		std::cout << arr2[10] << " = ";
-
-		arr3[10] = arr1[10] * arr2[10];
-		std::cout << arr3[10] << std::endl;
-
+		for (int col = 0; col < 3; col++) 
+		{ 
+			for (int inner = 0; inner < 2; inner++) 
+			{
+				product[row][col] += cMatrix[row][inner] * dMatrix[inner][col];
+			}
+			std::cout << product[row][col] << "  ";
+		}
+		std::cout << "\n";
 	}
-	getchar();
-	return 0;
 
 
 //скалярное произведение векторов
 	int Vector1[3], Vector2[3], res, n;
 	{
+		std::cin >> n;
 	for (int i = 0; i < n; i++)
 		{
 			res = Vector1[3] * Vector2[3];
